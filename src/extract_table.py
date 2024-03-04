b0221df9dec6d30a8852be6c572e709c228a0ecd
jrings <jrings@gmail.com>
2024-03-03 16:11:02 -0800

    Debugging to get a good result on 4054923.pdf

--- src/extract_table.py
+++ src/extract_table.py
@@ -1,10 +1,10 @@
+import itertools
 import sys
-import os
 
 import pandas as pd
 import pdfplumber
 
-from utils import log
+from utils import log, tonum
 
 COLUMNS = ["Emission Source", "Air Contaminant Name", "Emission Rate lbs/hr", "Emission Rate tons/year"]
 
@@ -23,12 +23,6 @@ def extract_from_text(text):
     end = [i for i, line in enumerate(lines) if i > start and "emission point identification" in line.lower()][0]
 
     rows = [line.strip().split() for line in lines[start:end]]
-    print(rows)
-    def tonum(x):
-        try:
-            return float(x)
-        except:
-            return float("nan")
         
     new_rows = []
     for rr in rows:
@@ -36,37 +30,51 @@ def extract_from_text(text):
             log.debug(f"Ignoring short row {rr} of length {len(rr)}")
         else:
             new_rows.append((" ".join(rr[:-3]), rr[-3], tonum(rr[-2]), tonum(rr[-1])))
-    last_non_none = None
-    rows = []
-    for row in new_rows:
-        if row[0] is not None:
-            last_non_none = row[0]
-        elif row[0] is None:
-            row[0] = last_non_none
-        rows.append(row)
-    df = pd.DataFrame(rows, columns=COLUMNS)
+
+    df = pd.DataFrame(new_rows, columns=COLUMNS)
     df["Emission Source"].replace('', pd.NA, inplace=True)
     df["Emission Source"].fillna(method="ffill", inplace=True)
     return df
 
 
-def extract_from_file(fname: str) -> pd.DataFrame:
+def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+        
+    df["Emission Source"].replace('', pd.NA, inplace=True)
+    df["Emission Source"].fillna(method="ffill", inplace=True)
+    df["Air Contaminant Name"] = df["Air Contaminant Name"].str.replace("\n", "_")
+    df = df[~df["Emission Rate tons/year"].str.contains('[a-zA-Z]', na=False)]
+    df = df[~df["Emission Source"].isin([": None"])]
+    return df
+
+def extract_from_file(fname: str, out_fname: str = "") -> pd.DataFrame:
     log.info(f"Extracting from {fname}")
     pdf = pdfplumber.open(fname)
-    try:
-        rows = pdf.pages[0].extract_table()[2:]
+    table_segments = []
+    for p in pdf.pages:
+        try:
+            table_segments.append(p.extract_table())
+        except TypeError:
+            pass
+    table_segments = [t for t in table_segments if t is not None]
+    if not len(table_segments):
+        log.info("Encountered table without vertical lines, extracting from text")
+        text = "\n".join([p.extract_text() for p in pdf.pages])
+        df = extract_from_text(text)
+    else:
+        table_segments = [
+            t[2:] if "emission" in str(t[0]).lower() else t for t in table_segments
+        ]
+        rows = list(itertools.chain(*table_segments))
         log.debug("Encountered table with vertical lines")
         rows = [[f"{row[0]}: {row[1]}"]  + row[2:] if row[0] is not None else [""] + row[2:] for row in rows]
         df = pd.DataFrame(rows, columns=COLUMNS)
-        df["Emission Source"].replace('', pd.NA, inplace=True)
-        df["Emission Source"].fillna(method="ffill", inplace=True)
-    except TypeError:
-        log.debug("Encountered table without vertical lines, extracting from text")
-        text = "\n".join([p.extract_text() for p in pdf.pages])
-        df = extract_from_text(text)
+    df = clean_dataframe(df)
     log.debug(f"Extracted table has shape {df.shape}")
+    if not out_fname:
+        out_fname = fname.replace(".pdf", ".csv")
+    df.to_csv(out_fname, index=False)
     return(df)
 
 
 if __name__ == "__main__":
-    print(extract_from_file(sys.argv[1]))
+    extract_from_file(sys.argv[1])
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,18 @@
 import loguru
+import os
 import sys
 
 log = loguru.logger
 log.remove()
 log.add(
     sys.stdout,
+    level=os.environ.get("LOG_LEVEL", "DEBUG"),
     colorize=True,
     format="<m>{time:HH:mm:ss} [{level}]</m> <c>{module}:{function}:{line} | </c><level>{message}</level>",
 )
+
+def tonum(x):
+    try:
+        return float(x)
+    except:
+        return float("nan")
